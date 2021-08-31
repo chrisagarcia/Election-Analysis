44ccebaab20df440a7bddadee7a52d6bdf7c5d9b
Chris <cgarciaguy@gmail.com>
2021-08-31 11:09:18 -0500

    Create Nested_Conditionals.py

---
+++ practice/Nested_Conditionals.py
@@ -0,0 +1,14 @@
+
+score = int(input('what is your score? '))
+
+if score >= 90:
+    print('your grade is an A')
+elif score >= 80:
+    print('your grade is an B')
+elif score >= 70:
+    print('your grade is an C')
+elif score >= 60:
+    print('your grade is an D')
+else:
+    print('your grade is an F')
+
