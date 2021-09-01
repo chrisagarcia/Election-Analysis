7e191971e0677aebbd3d34d182c26695ac734546
Chris <cgarciaguy@gmail.com>
2021-09-01 12:37:13 -0500

    Update PyPoll.py

--- PyPoll.py
+++ PyPoll.py
@@ -2,7 +2,7 @@
 # what we need:
 #   -total number of votes cast
 #       *create a candidate_votes{} dictionary
-#       *for loop that runs thru to find candidates
+#       *for loop that runs through to find candidates
 #           it can maybe use (last_row - first_row + 1) for each candidate's vote_total
 
 #   -list of candidates with votes
@@ -25,6 +25,12 @@ import os
 file_to_load = os.path.join('Resources', 'election_results.csv')
 file_to_save = os.path.join('analysis', 'election_analysis.txt')
 
+# initializing an accumulator for votes
+total_votes = 0
+
+# list to hold names + candidate_votes dictionary
+candidate_options = []
+candidate_votes = {}
 
 # open file with the election data calling it election_data
 with open(file_to_load) as election_data:
@@ -33,16 +39,61 @@ with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
     # printing and skipping the header row
     headers = next(file_reader)
-    print(headers)
+    print(', '.join(headers))
+
+    # for-loop to read data
+    for row in file_reader:
+
+        # find candidate names + init the dict
+        candidate_name = row[2]
+        if candidate_name not in candidate_options:
+            candidate_options.append(candidate_name)
+            candidate_votes[candidate_name] = 0
+
+        # increment candidate_votes by 1 when passed over
+        candidate_votes[candidate_name] += 1
+
+        # accumulate total votes
+        total_votes += 1
+
+    print(f'total votes cast: {total_votes}\n')
+
+    # winner summary variable set
+    winning_candidate = ''
+    winning_count = 0
+    winning_percentage = 0
+
+    # loop through the candidates in candidate_options[]
+    candidate_percent = []
+    for i in candidate_options:
+
+        # find and print the percentage of total votes
+        votes = candidate_votes[i]
+        candidate_percent = 100 * float(votes) / float(total_votes)
+        print(f'{i} :: {candidate_percent: .1f}% ({votes:,})\n---------------------------------------')
+
+        # find and declare a winner
+        if (votes > winning_count) and (candidate_percent > winning_percentage):
+            winning_count = votes
+            winning_percentage = candidate_percent
+            winning_candidate = i
+
+    winner_summary = (
+        '\n\n\n'
+        f'________WINNER_ALERT_______________________\n'
+        f'Winner::             {winning_candidate}\n'
+        f'Winning Vote Count:: {winning_count:,}\n'
+        f'Winning Percent::    {winning_percentage:.1f}%\n'
+        f'___________________________________________'
+    )
+    print(winner_summary)
 
 
 # write to file
 with open(file_to_save, 'w') as outfile:
 
     outfile.write('Counties in the Election\n-------------------------\n')
-    counties_list = ['Arapahoe', 'Denver', 'Jefferson']
-    for i in counties_list:
-        outfile.write(i)
-        outfile.write('\n')
+    counties =['Arapahoe', 'Denver', 'Jefferson']
+    outfile.write(', '.join(counties))
 
 
